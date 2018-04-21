f87d41b213666b8d5f1b240e0ae27e917078c6ee
Yuzhe <yuzhelu@gmail.com>
2018-04-21 15:46:35 -0600

    lab1

--- src/components/movie.js
+++ src/components/movie.js
@@ -19,7 +19,7 @@ class Movie extends Component {
         const ActorInfo = ({actors}) => {
             return actors.map((actor, i) =>
                 <p key={i}>
-                    <b>{actor.actorName}</b> {actor.characterName}
+                    <b>{actor.ActorName}</b> {actor.CharacterName}
                 </p>
             );
         };
