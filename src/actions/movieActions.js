d38881964144a8a5ed4394860c066e50e0f0eaba
Yuzhe <yuzhelu@gmail.com>
2018-04-21 15:42:56 -0600

    lab1

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -55,7 +55,7 @@ export function fetchMovies(){
 export function fetchMovie(movieId){
     const env = runtimeEnv();
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/${movieId}?reviews=true`, {
+        return fetch(`${env.REACT_APP_API_URL}/{movieId}?reviews=true`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
